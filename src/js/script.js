cebd89b46acb2bc36bf9465daac9aaf14da6f2c4
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-08-29 17:30:18 +0900

    common

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,9 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+//ドロワーメニュー
+  $("#menu_btn").click(function () {
+    $("#sp_nav").toggleClass("js-open");
+    $(".menu-trigger>span").toggleClass("js-open");
+    $("#menu_btn").toggleClass("js-open");
+  });
 
 });
